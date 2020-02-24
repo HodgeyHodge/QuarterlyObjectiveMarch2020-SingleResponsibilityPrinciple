b64e13370113ed34e5b94e02bfe2971f265aeb46
Christopher Hodge <christopher@hodgey.co.uk>
2020-02-24 23:39:36 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_Single_Responsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException("Haven't implemented anything to demonstrate the Single Responsibility Principle yet!");
+        }
+    }
+}
