116d12b70cf6778b62d5cffdd811a66f8c2ff02d
Christopher Hodge <christopher@hodgey.co.uk>
2020-02-25 12:46:49 +0000

    do it the wrong way

---
+++ Person.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace _1_Single_Responsibility
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,43 @@ namespace _1_Single_Responsibility
     {
         static void Main(string[] args)
         {
-            throw new NotImplementedException("Haven't implemented anything to demonstrate the Single Responsibility Principle yet!");
+            /*
+                This is an example of a class which egregiously violates the Single Responsibility Principle.
+                This one class deals with:
+                    messages to the user
+                    input from the user
+                    validation
+                    username generation
+                If any one of these things has to change, Program.cs has to be edited!
+            */
+
+            //Welcome message
+            Console.WriteLine("Welcome to Hodgey's Username Generator Console App!");
+
+            //Get required user information
+            Person user = new Person();
+            Console.Write("Please give your first name: ");
+            user.FirstName = Console.ReadLine();
+            Console.Write("Please give your last name: ");
+            user.LastName = Console.ReadLine();
+
+            //Validate user information
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("Invalid first name given!");
+                Console.ReadLine();
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("Invalid last name given!");
+                Console.ReadLine();
+                return;
+            }
+            
+            //Generate Username
+            Console.WriteLine($"Your Username is: {user.FirstName.Substring(0, 1)}{user.LastName}{(new System.Random()).Next(0, 9)}");
+            Console.ReadLine();
         }
     }
 }
